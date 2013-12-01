f9fff12ca950f9e15de12ab64fd2a48721cbc670
Jeffrey Diaz <jeffreydiaz27@gmail.com>
2013-12-01 18:25:39 -0500

    Completed first iteration of our array based stack. Still needs testing.

---
+++ structures/JDArrayStack.c
@@ -0,0 +1,97 @@
+#include <assert.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "JDArrayStack.h"
+
+#define ARRAY_LENGTH 8
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif 
+
+/* Called when our array has no space for an additional element. */
+/* Will increase the size of array by nsize. */
+static void JDArrayStack_resize(struct JDArrayStack* s, size_t nsize);
+
+void JDArrayStack_init(struct JDArrayStack* s, size_t element_size, void(*destructor)(void*))
+{
+	s->count = 0;
+	s->elem_size = element_size;
+	s->array_length = ARRAY_LENGTH;
+	s->dealloc_elements = destructor; // An example for where this is useful: an aggregate type with dynamic memory.
+	s->elem = malloc(ARRAY_LENGTH * element_size);
+	
+	assert(s->elem != NULL);
+}
+
+void JDArrayStack_dealloc(struct JDArrayStack* s)
+{
+	assert((s != NULL) && (s->elem != NULL));
+	
+	int i;	
+	if (s->dealloc_elements != NULL) {
+		for (i = 0; i < s->count; ++i) {
+			void* elem_addr = (char*)s->elem + (s->elem_size * i);
+			s->dealloc_elements(elem_addr);
+		}
+	}
+
+	s->count = 0;
+	s->elem_size = 0;
+	s->array_length = 0;
+	s->dealloc_elements = NULL;
+
+	free(s->elem);
+	s->elem = NULL;
+}
+
+void JDArrayStack_push(struct JDArrayStack* s, void* value)
+{
+	assert((s != NULL) && (s->elem != NULL));
+	
+	if (s->count == s->array_length) 
+		JDArrayStack_resize(s, s->array_length * 2);
+	
+	void* top = (char*)s->elem + (s->elem_size * s->count);
+	memcpy(top, value, s->elem_size);
+	s->count++;
+}
+
+void JDArrayStack_pop(struct JDArrayStack* s, void* target)
+{
+	assert((s != NULL) && (s->elem != NULL));
+	assert(s->count > 0);
+
+	s->count--;
+	void* top = (char*)s->elem + (s->elem_size * s->count);
+	memcpy(target, top, s->elem_size);
+}
+
+int JDArrayStack_empty(struct JDArrayStack* s)
+{
+	assert((s != NULL) && (s->elem != NULL));
+
+	if (s->count > 0) return FALSE;
+	return TRUE;
+}
+
+int JDArrayStack_count(struct JDArrayStack* s)
+{
+	assert((s != NULL) && (s->elem != NULL));
+	
+	return s->count;
+}
+
+static void JDArrayStack_resize(struct JDArrayStack* s, size_t nsize)
+{
+	s->elem = realloc(s->elem, nsize * s->elem_size);
+	
+	assert(s->elem != NULL);
+	s->array_length = nsize;
+}
---
+++ structures/JDArrayStack.h
@@ -0,0 +1,45 @@
+/*
+ * A generic stack using an array of a initial size 8.
+ *
+ * Date: 12/01/2013
+ */
+
+#ifndef JDARRAYSTACK_H
+#define JDARRAYSTACK_H
+
+#include <stddef.h>
+
+struct JDArrayStack {
+	size_t array_length;            /* Keep a count of our array length for resizing purposes. */
+	size_t count;                   /* Keep a count of the number of elements in stack.        */
+	size_t elem_size;               /* We must know the size of the elements in our stack.     */ 
+	void*  elem;                    /* A pointer to our dynamically allocated array.           */
+	void(*dealloc_elements)(void*); /* A client provided destrutor for complex elements.       */
+};
+
+/*********************************************************************************/
+/* Function prototypes for the creation and destruction of a JDStack. */
+
+/* initialize a JDStack with an initial alloc size of 8 elements. */
+/* Can optionally pass a function pointer for deallocation of a more complex type*/
+void JDArrayStack_init(struct JDArrayStack* s, size_t element_size, void(*)(void*));
+
+/* destroy a JDStack. */
+void JDArrayStack_dealloc(struct JDArrayStack* s);
+
+/*********************************************************************************/
+/* Function prototypes for the usual operations we need to perform on a stack */
+
+/* push a value onto our stack. */
+void JDArrayStack_push(struct JDArrayStack* s, void* value);
+
+/* pop a value from our stack and copy into a dest variable. */
+void JDArrayStack_pop(struct JDArrayStack* s, void* dest);
+
+/* returns TRUE(1) if our stack is empty or FALSE(0) if it's not. */
+int JDArrayStack_empty(struct JDArrayStack* s);
+
+/* returns the number of elements currently in our stack. */
+int JDArrayStack_count(struct JDArrayStack* s);
+
+#endif
