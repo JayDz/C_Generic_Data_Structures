0bad0fa8eadb72842e9f91e417347b0743cf961b
Jeffrey Diaz <jeffreydiaz27@gmail.com>
2013-12-20 03:19:05 -0500

    Created header file containing common error codes returned by various data structure functions.

---
+++ structures/JDError_codes.h
@@ -0,0 +1,23 @@
+/*
+ * This header file contains constants representing errors and feedback to client software.
+ * 12/17/13
+*/
+
+#ifndef JDERRORCODE_H
+#define JDERRORCODE_H
+
+#define JD_FMALLOC	0x00
+#define JD_NULLPTR	0x00
+#define JD_NULLPTR_ARG	0x01
+#define JD_EMPTY	0x02
+#define JD_SUCCESS	0x03
+
+#ifndef TRUE 
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#endif
