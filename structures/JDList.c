729d2b9469baa0657f4078f8808bb1d0099947bd
Jeffrey Diaz <jeffreydiaz27@gmail.com>
2013-12-31 02:52:12 -0500

    Completed first version of doubly linked list.

---
+++ structures/JDList.c
@@ -0,0 +1,267 @@
+/*
+ * Implementation of a generic doubly linked list. 
+ */
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "JDList.h"
+
+static int make_node(struct JDTwoWayNode** n, void* value, size_t elem_size)
+{
+	*n = malloc(sizeof(struct JDTwoWayNode));
+	if (*n == NULL) return JD_FMALLOC;
+
+	(*n)->value = malloc(elem_size);
+	if ((*n)->value == NULL) {
+		free(*n);
+		return JD_FMALLOC;
+	}
+	memcpy((*n)->value, value, elem_size);
+	
+	return JD_SUCCESS;
+}
+
+int JDList_init(struct JDList* list, size_t elem_size, void(*dtor)(void*))
+{
+	if (list == NULL) return JD_NULLPTR;
+
+	list->head = NULL;
+	list->tail = NULL;
+	list->dtor = dtor;
+	list->count = 0;
+	list->elem_size = elem_size;
+
+	return JD_SUCCESS;
+}
+
+int JDList_dealloc(struct JDList* list)
+{
+	if (list == NULL) return JD_NULLPTR;
+
+	int i;
+	for (i = 0; i < list->count; ++i) {
+		if (list->dtor) 
+			list->dtor(list->head->value);
+		struct JDTwoWayNode* tmp = list->head;
+		list->head = list->head->next;
+		free(tmp->value);
+		free(tmp);
+	}
+
+	list->head = NULL;
+	list->tail = NULL;
+	list->dtor = NULL;
+	list->count = 0;
+	list->elem_size = 0;
+
+	return JD_SUCCESS;
+}
+
+int JDList_append(struct JDList* list, void* item)
+{
+	if (list == NULL) return JD_NULLPTR;
+	if (item == NULL) return JD_NULLPTR_ARG;
+
+	struct JDTwoWayNode* n;
+	int ret = make_node(&n, item, list->elem_size);
+	if (ret == JD_FMALLOC) return ret;
+
+	n->next = NULL;
+	n->prev = NULL;
+	if (list->count) {
+		n->prev = list->tail;
+		list->tail->next = n;
+		list->tail = n;
+	} else {
+		list->head = n;
+		list->tail = n;
+	}
+
+	list->count++;
+
+	return JD_SUCCESS;
+}
+
+int JDList_prepend(struct JDList* list, void* item)
+{
+	if (list == NULL) return JD_NULLPTR;
+	if (item == NULL) return JD_NULLPTR_ARG;
+
+	struct JDTwoWayNode* n;
+	int ret = make_node(&n, item, list->elem_size);
+	if (ret == JD_FMALLOC) return ret;
+
+	n->next = NULL;
+	n->prev = NULL;
+	if (list->count) {
+		list->head->prev = n;
+		n->next = list->head;
+		list->head = n;
+	} else {
+		list->head = n;
+		list->tail = n;
+	}
+
+	list->count++;
+
+	return JD_SUCCESS;
+}
+
+int JDList_front(struct JDList* list, void* dest)
+{
+	if (list == NULL) 	return JD_NULLPTR;
+	if (dest == NULL) 	return JD_NULLPTR_ARG;
+	if (!(list->count)) 	return JD_EMPTY;
+
+	memcpy(dest, list->head->value, list->elem_size);
+
+	return JD_SUCCESS;
+}
+
+int JDList_back(struct JDList* list, void* dest)
+{
+	if (list == NULL) 	return JD_NULLPTR;
+	if (dest == NULL) 	return JD_NULLPTR_ARG;
+	if (!(list->count)) 	return JD_EMPTY;
+
+	memcpy(dest, list->tail->value, list->elem_size);
+
+	return JD_SUCCESS;
+}
+
+int JDList_remove(struct JDList* list, void* dest)
+{
+	if (list == NULL) 	return JD_NULLPTR;
+	if (dest == NULL) 	return JD_NULLPTR_ARG;
+	if (!(list->count)) 	return JD_EMPTY;
+
+	struct JDTwoWayNode* tmp = list->head;
+	list->head = list->head->next;
+	memcpy(dest, tmp->value, list->elem_size);
+	free(tmp->value);
+	free(tmp);
+
+	list->count--;
+
+	return JD_SUCCESS;
+}
+
+int JDList_remove_back(struct JDList* list, void* dest)
+{
+	if (list == NULL) 	return JD_NULLPTR;
+	if (dest == NULL) 	return JD_NULLPTR_ARG;
+	if (!(list->count)) 	return JD_EMPTY;
+
+	memcpy(dest, list->tail->value, list->elem_size);
+	struct JDTwoWayNode* tmp = list->tail->prev;
+	free(list->tail->value);
+	free(list->tail);
+	list->tail = tmp;
+
+	list->count--;
+
+	return JD_SUCCESS;
+}
+
+int JDList_remove_if(struct JDList* list, int(*predicate)(void*))
+{
+	if (list == NULL) return JD_NULLPTR;
+	if (predicate == NULL) return JD_NULLPTR_ARG;
+
+	struct JDTwoWayNode* prev_head = NULL;
+	struct JDTwoWayNode* curr_node = list->head;
+	int matches = 0;
+
+	int i;
+	for (i = 0; i < list->count; ++i) {
+		if (prev_head == NULL) 
+			if (list->head != curr_node)
+				prev_head = list->head;
+
+		if (predicate(curr_node->value)) {
+			if (list->head == curr_node) {
+				list->head = list->head->next;
+				list->head->prev = NULL;
+				if (list->dtor) list->dtor(curr_node->value);
+				free(curr_node->value);
+				free(curr_node);
+				curr_node = list->head;
+			} else if (i == ((list->count)-1)) {
+				list->tail = prev_head;
+				if (list->dtor) list->dtor(curr_node->value);
+				free(curr_node->value);
+				free(curr_node);
+			} else {
+				prev_head->next = curr_node->next;
+				curr_node->prev = prev_head;
+				if (list->dtor) list->dtor(curr_node->value);
+				free(curr_node->value);
+				free(curr_node);
+				curr_node = prev_head->next;
+			}
+
+			matches++;
+		} else {
+			struct JDTwoWayNode* tmp = curr_node;
+			curr_node = curr_node->next;
+			prev_head = tmp;
+		}
+	}
+
+	list->count -= matches;
+
+	return JD_SUCCESS;
+}
+
+int JDList_array(struct JDList* list, void* dest)
+{
+	if (list == NULL) 	return JD_NULLPTR;
+	if (dest == NULL) 	return JD_NULLPTR_ARG;
+	if (!(list->count)) 	return JD_EMPTY;
+
+	int i;
+	struct JDTwoWayNode* curr = list->head;
+	for (i = 0; i < list->count; ++i) {
+		void* addr = (char*)dest + (list->elem_size * i);
+		memcpy(addr, curr->value, list->elem_size);
+		curr = curr->next;
+	}
+
+	return JD_SUCCESS;
+}
+
+int JDList_count(struct JDList* list)
+{
+	if (list == NULL) return JD_NULLPTR;
+	return list->count;
+}
+
+int JDList_empty(struct JDList* list)
+{
+	if (list == NULL) return TRUE;
+	if (list->count)
+		return FALSE;
+	return TRUE;
+}
+
+int JDList_contains(struct JDList* list, void* key, int(*cmp)(void*, void*))
+{
+	if (list == NULL) 	return FALSE;
+	if (key  == NULL) 	return FALSE;
+	if (!(list->count)) 	return FALSE;
+	
+	int i;
+	struct JDTwoWayNode* curr = list->head;
+	for (i = 0; i < list->count; ++i) {
+		if (cmp) {
+			if (!cmp(curr->value, key)) return TRUE;
+		} else {
+			if (!memcmp(curr->value, key, list->elem_size)) return TRUE;
+		}
+		curr = curr->next;
+	}
+
+	return FALSE;
+}
---
+++ structures/JDList.h
@@ -0,0 +1,66 @@
+/*
+ * Header file for a generic doubly linked list.
+ */
+
+#ifndef JDLIST_H
+#define JDLIST_H
+
+#include <stddef.h>
+#include "JDError_codes.h"
+
+struct JDTwoWayNode {
+	void* value;			/* Pointer to node data. */
+	struct JDTwoWayNode* next;	/* Pointer to next node. */
+	struct JDTwoWayNode* prev;	/* Pointer to prev node. */
+};
+
+struct JDList {
+	struct JDTwoWayNode* head;	/* Pointer to the first node in list. */
+	struct JDTwoWayNode* tail;	/* Pointer to the last node in list.  */
+	int count;			/* The number of nodes in list.       */
+	size_t elem_size;		/* The size of value being stored.    */
+	void(*dtor)(void*);		/* Pointer to a value destructor.     */
+};
+
+/******************************************************************************/
+
+/* Prepares a JDList for operations. */
+int JDList_init(struct JDList* list, size_t elem_size, void(*dtor)(void*));
+
+/* Destroys a list, if a destructor was passed to init, it will be called for every node in list. */
+int JDList_dealloc(struct JDList* list);
+
+/* Add an item to back of list. */
+int JDList_append(struct JDList* list, void* item);
+
+/* Add an item to front of list. */
+int JDList_prepend(struct JDList* list, void* item);
+
+/* Look at first item of list, does not remove it. */
+int JDList_front(struct JDList* list, void* dest);
+
+/* Look at last item of list, does not remove it. */
+int JDList_back(struct JDList* list, void* dest);
+
+/* Remove the first item in list. */
+int JDList_remove(struct JDList* list, void* dest);
+
+/* Remove the last item in list. */
+int JDList_remove_back(struct JDList* list, void* dest);
+
+/* Remove all nodes who satisfy predicate. */
+int JDList_remove_if(struct JDList* list, int(*predicate)(void*));
+
+/* Copy all values in nodes into an array. List maintains ownership. */
+int JDList_array(struct JDList* list, void* dest);
+
+/* Return the number of nodes in list. */
+int JDList_count(struct JDList* list);
+
+/* Test whether list is empty. */
+int JDList_empty(struct JDList* list);
+
+/* Returns true if key is found in list. */
+int JDList_contains(struct JDList* list, void* key, int(*cmp)(void*,void*));
+
+#endif
