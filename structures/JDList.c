96c437fa1edb5609e78f6ffc2857a351b1d23b05
Jeffrey Diaz <jeffreydiaz27@gmail.com>
2014-01-02 15:29:35 -0500

    Added before insert/remove functionality.

--- structures/JDList.c
+++ structures/JDList.c
@@ -5,7 +5,6 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "JDList.h"
 
 static int make_node(struct JDTwoWayNode** n, void* value, size_t elem_size)
@@ -264,4 +263,157 @@ int JDList_contains(struct JDList* list, void* key, int(*cmp)(void*, void*))
 	}
 
 	return FALSE;
+}
+
+int JDList_insert_after(struct JDList* list, void* key, void* value, int(*cmp)(void*,void*))
+{
+	if (list == NULL) 	return JD_NULLPTR;
+	if (key == NULL) 	return JD_NULLPTR_ARG;
+	if (value == NULL) 	return JD_NULLPTR_ARG;
+	if (cmp == NULL)	return JD_NULLPTR_ARG;
+
+	struct JDTwoWayNode* curr = list->head;
+	int i;
+	for (i = 0; i < list->count; ++i) {
+		if (!cmp(curr->value, key)) {
+			if (i == 0) {
+				return JDList_append(list, value);
+			} else if (i == (list->count - 1)) {
+				struct JDTwoWayNode* n;
+				int ret = make_node(&n, value, list->elem_size);
+				if (ret == JD_FMALLOC) return ret;
+
+				curr->next = n;
+				n->prev = curr;
+				list->tail = n;
+				list->count++;
+				return JD_SUCCESS;
+			} else {
+				struct JDTwoWayNode* n;
+				int ret = make_node(&n, value, list->elem_size);
+				if (ret == JD_FMALLOC) return ret;
+				
+				n->next = curr->next;
+				curr->next->prev = n;
+				n->prev = curr;
+				curr->next = n;
+				list->count++;
+				return JD_SUCCESS;
+			}
+		}
+		curr = curr->next;
+	}
+
+	return JD_NOTFOUND;
+}
+
+int JDList_remove_after(struct JDList* list, void* key, int(*cmp)(void*,void*))
+{
+	if (list == NULL)	return JD_NULLPTR;
+	if (key == NULL)	return JD_NULLPTR_ARG;
+	if (cmp == NULL)	return JD_NULLPTR_ARG;
+
+	struct JDTwoWayNode* curr = list->head;
+	int i;
+	for (i = 0; i < list->count; ++i) {
+		if (!cmp(curr->value, key)) {
+			if (list->head == list->tail) {
+				return JD_SUCCESS;
+			} else if (curr == list->tail) {
+				return JD_SUCCESS;
+			} else if (i == (list->count - 2)) {
+				if (list->dtor) 
+					list->dtor(list->tail->value);
+				free(list->tail->value);
+				free(list->tail);
+				list->tail = curr;
+				list->count--;
+				return JD_SUCCESS;
+			} else {
+				struct JDTwoWayNode* tmp = curr->next;
+				curr->next = tmp->next;
+				tmp->next->prev = curr;
+				if (list->dtor)
+					list->dtor(tmp->value);
+				free(tmp->value);
+				free(tmp);
+				list->count--;
+				return JD_SUCCESS;
+			}
+		}
+		curr = curr->next;
+	}
+
+	return JD_NOTFOUND;
+}
+
+int JDList_insert_before(struct JDList* list, void* key, void* value, int(*cmp)(void*,void*))
+{
+	if (list == NULL) 	return JD_NULLPTR;
+	if (key == NULL) 	return JD_NULLPTR_ARG;
+	if (value == NULL) 	return JD_NULLPTR_ARG;
+	if (cmp == NULL) 	return JD_NULLPTR_ARG;
+
+	struct JDTwoWayNode* curr = list->head;
+	int i;
+	for (i = 0; i < list->count; ++i) {
+		if (!cmp(curr->value, key)) {
+			if (i == 0) {
+				return JDList_prepend(list,value);
+			} else {
+				struct JDTwoWayNode* n;
+				int ret = make_node(&n, value, list->elem_size);
+				if (ret == JD_FMALLOC) return ret;
+				n->prev = curr->prev;
+				struct JDTwoWayNode* tmp = curr->prev;
+				tmp->next = n;
+				n->next = curr;
+				curr->prev = n;
+				list->count++;
+				return JD_SUCCESS;
+			}
+		}
+		curr = curr->next;
+	}
+
+	return JD_NOTFOUND;
+}
+
+int JDList_remove_before(struct JDList* list, void* key, int(*cmp)(void*,void*))
+{
+	if (list == NULL) 	return JD_NULLPTR;
+	if (key == NULL) 	return JD_NULLPTR_ARG;
+	if (cmp == NULL)	return JD_NULLPTR_ARG;
+
+	struct JDTwoWayNode* curr = list->head;
+	int i;
+	for (i = 0; i < list->count; ++i) {
+		if (!cmp(curr->value, key)) {
+			if (i == 0) {
+				return JD_SUCCESS;
+			} else if (i == 1) {
+				if (list->dtor)
+					list->dtor(list->head->value);
+				free(list->head->value);
+				free(list->head);
+				list->head = curr;
+				list->count--;
+				return JD_SUCCESS;
+			} else {
+				struct JDTwoWayNode* kill = curr->prev;
+				curr->prev = kill->prev;
+				struct JDTwoWayNode* tmp = kill->prev;
+				tmp->next = curr;
+				if (list->dtor)
+					list->dtor(kill->value);
+				free(kill->value);
+				free(kill);
+				list->count--;
+				return JD_SUCCESS;
+			}
+		}
+		curr = curr->next;
+	}
+
+	return JD_NOTFOUND;
 }
